0e27d962f7ce1955895f60ba557b0493d0fc43c9
Andrea Gemma <andrea.gemma@gmail.com>
2026-09-23 19:28:38 +0200

    Remove unused files and code for the dictparser project
    - Deleted the LICENSE file for the twine package as it is no longer needed.
    - Removed the summary.tsv file that listed third-party packages and their licenses.
    - Cleared the pyproject.toml file, removing project metadata and dependencies.
    - Deleted the quality_tests.sh script that checked code quality.
    - Removed the __init__.py, __main__.py, _version.py, and dictparser.py files, which contained the main functionality of the dictparser library.
    - Deleted the py.typed file that indicated type information for the package.
    - Removed all test files (test_basic.py and test_version.py) that were used for testing the dictparser functionality.

--- src/dictparser/__init__.py
+++
@@ -1,8 +0,0 @@
-"""template package
-Simple library functions for the project.
-"""
-
-from ._version import __version__
-from .dictparser import DictParser
-
-__all__ = ["__version__", "DictParser"]
